2e192e1c02523df1ce3fb6bbe221ade3503c751e
opiyobrayan <67815971+opiyobrayan@users.noreply.github.com>
2022-09-27 15:57:13 +0300

    Update settings.py

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -126,3 +126,5 @@ STATICFILES_DIR=[
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['https://*.railway-production-fe80.up.railway.app/']
